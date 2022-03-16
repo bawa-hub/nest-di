2b0317ebfdb8e143e0d48a6a23a5c2cebb3a7b6f
bawa-hub <itsworkingmind@gmail.com>
2022-03-16 19:54:20 +0530

    initial commit

--- src/disk/disk.module.ts
+++ src/disk/disk.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PowerModule } from 'src/power/power.module';
+import { DiskService } from './disk.service';
 
-@Module({})
+@Module({
+  imports: [PowerModule],
+  providers: [DiskService],
+  exports: [DiskService],
+})
 export class DiskModule {}
